6f0a8d08c2d4833fee546d838bbb8f2c91708063
YOUGIL Kamal <kamal.yougil@outlook.fr>
2025-04-28 11:15:53 +0200

    feat: adding event tables with FK

--- config/donkeyEvent.sql
+++ config/donkeyEvent.sql
@@ -19,3 +19,34 @@ CREATE TABLE users (
   role_id INT,
   CONSTRAINT FK_roleid FOREIGN KEY (role_id) REFERENCES roles(role_id) ON DELETE CASCADE
 );
+
+CREATE TABLE country(
+  country_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  name VARCHAR(100)
+);
+
+CREATE TABLE city(
+  city_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  country_id INT,
+  name VARCHAR(70),
+  CONSTRAINT FK_country_id FOREIGN KEY (country_id) REFERENCES country(country_id) ON DELETE CASCADE
+
+);
+
+CREATE TABLE category(
+  category_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  name VARCHAR(100)
+);
+
+
+CREATE TABLE events (
+  id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  city_id INT,
+  category_id INT,
+  date_event DATE,
+  name VARCHAR(60),
+  price FLOAT,
+  CONSTRAINT FK_city_id FOREIGN KEY (city_id) REFERENCES city(city_id) ON DELETE CASCADE,
+  CONSTRAINT FK_category_id FOREIGN KEY (category_id) REFERENCES category(category_id) ON DELETE CASCADE
+);
+
