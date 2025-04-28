21e9b222e332b4b6487e315a665249ad96bb6852
YOUGIL Kamal <kamal.yougil@outlook.fr>
2025-04-28 17:16:57 +0200

    feat: adding view login & register

---
+++ views/login.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="FR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <section>
+        <h1>DONKEY EVENT</h1>
+        <form action="" methode="post">
+            <input type="email" id="email" name="email" placeholder="Entrer votre e-mail">
+            <input type="password" id="password" name="password" placeholder="Votre mot de passe">
+            <button>Connexion</button>
+            <p>Vous n'avez pas de compte ? <a href="page2.html">inscrivez-vous</a> !</p>
+        </form>
+
+    </section>
+    
+</body>
+</html>
---
+++ views/register.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="FR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <section>
+        <h1>DONKEY EVENT</h1>
+        <form action="" methode="post">
+            <h2>Formulaire d'inscription</h2>
+            <input type="email" id="e-mail" name="e-mail" placeholder="Entrer votre e-mail">
+            <input type="text" id="mp" name="mp" placeholder="Votre mot de passe">
+            <select name="gender" id="gender-select">
+                <option value="">choisissez un genre</option>
+                <option value="male">Homme</option>
+                <option value="female">Femme</option>
+            </select>
+            <input type="text" id="nom" name="nom" placeholder="nom">
+            <input type="text" id="Prenom" name="Prenom" placeholder="Prenom">
+            <button>Valider</button>
+        </form>
+
+    </section>
+    
+</body>
+</html>
