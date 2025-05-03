7c6dcb5c2a8fe86ea4262763e82826b8ad4621c3
tbeaumont79 <tbeaumont79@icloud.com>
2025-05-03 16:50:03 +0200

    feat(model): update the model adding a getCurrentUser() method

--- models/model.php
+++ models/model.php
@@ -9,4 +9,16 @@ class Model
         $this->db = new Db();
         $this->pdo = $this->db->getPdo();
     }
+
+
+    private function getCurrentUser() {
+        session_start();
+        $id = $_SESSION['user_id'];
+        $sql = "SELECT * FROM users WHERE user_id = :id";
+        $stmt = $this->pdo->prepare($sql);
+        $user = $stmt->execute([
+            'id' => $id
+        ]);
+        return $user;
+    }
 }
