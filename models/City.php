f211bd522deb737490575acaa898d4bbdffeddc6
tbeaumont79 <tbeaumont79@icloud.com>
2025-05-05 17:02:53 +0200

    fix: import path

--- models/City.php
+++ models/City.php
@@ -1,46 +1,39 @@
 
 <?php
-
-require 'model.php';
-interface Crud {
-    public function create ();
-    public function read ();
-    public function update ();
-    public function delete ();
+require_once __DIR__ . '/model.php';
+interface Crud
+{
+    public function create();
+    public function read();
+    public function update();
+    public function delete();
 }
-  
 
-class City extends Model implements Crud {
-    public function create(){
-    
 
+class City extends Model implements Crud
+{
+    public function create() {}
+
+    public function __construct()
+    {
+        parent::__construct();
     }
 
-    public function __construct(){
-    parent::__construct();
+    public function read()
+    {
+        $sql = 'select * from city';
+        $query = $this->pdo->prepare($sql);
+        $query->execute();
+        $result = $query->fetchAll(PDO::FETCH_ASSOC);
+
+        var_dump($result);
+
+        return $result;
     }
 
-    public function read(){
-    $sql = 'select * from city';
-    $query = $this->pdo->prepare($sql);
-    $query->execute();
-    $result = $query->fetchAll(PDO::FETCH_ASSOC);
-
-    var_dump($result);
-
-    return $result;
-
-    }
-
-    public function update(){
-
-    }
-    public function delete(){
-
-    }
-
-} 
-
+    public function update() {}
+    public function delete() {}
+}
 $cityObjet = new City();
 $cityObjet->read();
 
