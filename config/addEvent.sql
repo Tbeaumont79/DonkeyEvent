a608f8e409b9bdd5687568d1111420e7d7063a38
tbeaumont79 <tbeaumont79@icloud.com>
2025-05-05 18:40:40 +0200

    fix: eventOptions sql + adding addEvent.sql

---
+++ config/addEvent.sql
@@ -0,0 +1,5 @@
+use donkeyevent;
+
+INSERT INTO events (city_id, category_id, option_id, date_event, name, price) VALUES (1, 1, 1, '2023-10-01', 'Concert de rock', 50.00);
+INSERT INTO events (city_id, category_id, option_id, date_event, name, price) VALUES (2, 2, 2, '2023-10-05', 'Pièce de théâtre', 30.00);
+INSERT INTO events (city_id, category_id, option_id, date_event, name, price) VALUES (3, 3, 3, '2023-10-10', 'Match de football', 20.00);
--- config/eventOptions.sql
+++ config/eventOptions.sql
@@ -1,4 +1,4 @@
 use donkeyevent;
-INSERT INTO options (name) VALUES ('Accès au bar', 15.00);
-INSERT INTO options (name) VALUES ('Atelier peinture', 45.00);
-INSERT INTO options (name) VALUES ('Place VIP concert', 20.00);
+INSERT INTO options (name, price) VALUES ('Accès au bar', 15.00);
+INSERT INTO options (name, price) VALUES ('Atelier peinture', 45.00);
+INSERT INTO options (name, price) VALUES ('Place VIP concert', 20.00);
