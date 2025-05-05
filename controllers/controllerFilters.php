e1bd3587a8e011e55f636237c6f955cb4d971bf9
tbeaumont79 <tbeaumont79@icloud.com>
2025-05-05 18:47:28 +0200

    feat(controllerFilter): update start method

--- controllers/controllerFilters.php
+++ controllers/controllerFilters.php
@@ -1,5 +1,20 @@
 <?php
+
+require_once __DIR__ . '../models/FiltersModel.php';
 class ControllerFilters
 {
     public function __construct() {}
+
+    public function start()
+    {
+        if (!isset($_POST['city']) || !isset($_POST['date']) || !isset($_POST['category']))
+            require_once('views/filters.php');
+        else {
+            $city = htmlentities($_POST['city']);
+            $date = htmlentities($_POST['date']);
+            $category = htmlentities($_POST['category']);
+            $filtersModel = new FiltersModel($city, $date, $category);
+            header('Location: views/liste.php');
+        }
+    }
 }
