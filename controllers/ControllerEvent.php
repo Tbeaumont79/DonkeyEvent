b130af9292a639bcdde5827725858649bd080cca
tbeaumont79 <tbeaumont79@icloud.com>
2025-05-06 11:25:37 +0200

    fix: use a header location instead of require_once

--- controllers/ControllerEvent.php
+++ controllers/ControllerEvent.php
@@ -8,7 +8,7 @@ class ControllerEvent
     public function start()
     {
         if (!isset($_POST['city']) || !isset($_POST['date']) || !isset($_POST['category'])) {
-            require_once(__DIR__ . '/../views/filters.php');
+            header('Location: index.php?page=filters');
         } else {
             $eventsModel = new EventModel($_POST['city'], $_POST['date'], $_POST['category']);
             $events = $eventsModel->getEvents();
