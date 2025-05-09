e7fbc344a6b1ccd93b82796ba4c934bca69a5b2d
tbeaumont79 <tbeaumont79@icloud.com>
2025-05-09 10:53:07 +0200

    fix: update the controller logout

--- src/controllers/ControllerLogout.php
+++ src/controllers/ControllerLogout.php
@@ -2,14 +2,20 @@
 
 namespace Thibaultbeaumont\DonkeyEvent\Controllers;
 
+use function PHPUnit\Framework\isEmpty;
+
 class ControllerLogout
 {
     public function __construct() {}
 
     public function start(): void
     {
-        if (isset($_SESSION['user'])) {
+
+        if (isset($_SESSION['user']) && !isEmpty($_SESSION['user'])) {
             session_destroy();
+            header('Location: index.php');
+            exit();
         }
+        header('Location: index.php');
     }
 }
