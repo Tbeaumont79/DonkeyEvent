f0e8ed960a747fc7e1f34586821e2841816977e5
tbeaumont79 <tbeaumont79@icloud.com>
2025-05-15 19:33:54 +0200

    feat(test): adding panther bundle to do e2e testing + creating a test to check the user create + create in db

---
+++ tests/TestCreateUserAddsUserToDatabase.php
@@ -0,0 +1,13 @@
+<?php
+
+require_once(__DIR__ . '/../vendor/autoload.php');
+
+use Symfony\Component\Panther\PantherTestCase;
+use Thibaultbeaumont\DonkeyEvent\Models\UserModel;
+use PHPUnit\Framework\TestCase;
+
+class TestCreateUserAddsUserToDatabase extends TestCase {
+    public function testCreateUser() {
+
+    }
+}
