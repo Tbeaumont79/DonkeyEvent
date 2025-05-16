2c2d5027bd61ba335d3233e0def80db7b16356dc
tbeaumont79 <tbeaumont79@icloud.com>
2025-05-16 12:13:59 +0200

    feat(test): adding the first e2e test to create user

--- tests/TestCreateUserAddsUserToDatabase.php
+++ tests/TestCreateUserAddsUserToDatabase.php
@@ -4,10 +4,23 @@ require_once(__DIR__ . '/../vendor/autoload.php');
 
 use Symfony\Component\Panther\PantherTestCase;
 use Thibaultbeaumont\DonkeyEvent\Models\UserModel;
-use PHPUnit\Framework\TestCase;
 
-class TestCreateUserAddsUserToDatabase extends TestCase {
-    public function testCreateUser() {
+class TestCreateUserAddsUserToDatabase extends PantherTestCase
+{
 
+    public function testCreateUser()
+    {
+        $client = static::createPantherClient([
+            'webServerDir' => __DIR__ . '/..'
+        ]);
+        $crawler = $client->request('GET', 'http://localhost:8000/index.php?page=register');
+        $crawler = $client->submitForm('Valider', [
+            "email" => "tbeaumont89@icloud.com",
+            "password" => "test123123",
+            "gender" => "male",
+            "lastname" => "thibault",
+            "firstname" => "beaumont"
+        ]);
+        $this->assertStringContainsString('/index.php?page=login', $client->getCurrentURL());
     }
 }
